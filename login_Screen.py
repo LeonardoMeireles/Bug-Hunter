0e855bd6c3d11d54d42f42e275a10defb552539f
Leonardo Meireles <62730338+LeonardoMeireles@users.noreply.github.com>
2021-03-16 00:50:02 -0300

    Add files via upload
    Nesta atualização foram adicionados:
    - mensagens de erro na hora de login e registro.
    - Um banco de dados para as informações dos usuários (ainda precisa ser aplicado um sistema de hashing e salting para segurança das senhas)
    - Funções para o botão de login e registro para verificar as entradas
        -> As funções para verificar se o e-mail ou o usuário já está sendo usado precisa ser arrumado.

--- login_Screen.py
+++ login_Screen.py
@@ -1,13 +1,30 @@
+import re
+
 import tkinter as tk
 from PIL import ImageTk, Image
 
-import sqlite3
+import mysql.connector
 
 #Color Palette: 
 teal_Green = "#028476"
 forest_Green = "#015D53"
 black = "#030303"
 lime_Green = "#6ECB5A"
+red = "#b80018"
+
+#Conectando ao Banco de Dados
+db = mysql.connector.connect(
+        host="remotemysql.com",
+        user="K31HAiTnbo",
+        password ="v36iANlNw8",
+        database = "K31HAiTnbo",
+        port = "3306"
+    )
+
+cursor = db.cursor()
+
+#Criando a Table para guardar informações de Login
+#cursor.execute("CREATE TABLE Users (user_id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(255), username VARCHAR(50),email VARCHAR(255),password VARCHAR(255))")
 
 class BugHutner(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -55,7 +72,7 @@ class LoginWindow(tk.Frame):
 
         #Titulo da página
         login_label = tk.Label(self, text = "Login", font = ("Montserrat", 44), bg = teal_Green)
-        login_label.grid(row = 0,  column = 0, columnspan = 3, sticky = 'w', padx = 85, pady = 10)
+        login_label.grid(row = 0,  column = 0, columnspan = 3, sticky = 'w', padx = 85, pady = (10,0))
 
         #Criando a entrada para o Username
         def holder_UText(event):
@@ -93,16 +110,16 @@ class LoginWindow(tk.Frame):
         password_in.insert(0, "Senha")
         password_in.bind("<FocusIn>", holder_PText)
         password_in.bind("<FocusOut>", holder_PText)
-        password_in.grid(row = 2, column = 0, columnspan = 2, sticky = 'n'+'w'+'e'+'s', padx = 32, pady = 12)
+        password_in.grid(row = 3, column = 0, columnspan = 2, sticky = 'n'+'w'+'e'+'s', padx = 32, pady = (20,30))
 
         self.password_Image = tk.PhotoImage(file = "Assets/password.png")
         password_icon = tk.Label(self, image = self.password_Image, bd = 0, padx = 100, background = teal_Green, activebackground  = teal_Green)
-        password_icon.grid(row = 2, column = 0, sticky = 'w')
+        password_icon.grid(row = 3, column = 0, sticky = 'w' + 'n', pady = (17,0))
 
         #Botão que redireciona para se registrar
         forgotP_btn = tk.Button(self, text = "Esqueci a senha", command = lambda: controller.show_frame("ForgotPWindow"),
                                 font = ("Montserrat",10), bg = teal_Green, fg = "#002e29", activebackground =  "#01695d",  borderwidth = 0)
-        forgotP_btn.grid(row = 3, column = 1, padx = 15, sticky = 'w')
+        forgotP_btn.grid(row = 5, column = 1, padx = 15, sticky = 'w')
 
         #Botão de lembrar desse usuário
         def remember_user():
@@ -110,20 +127,35 @@ class LoginWindow(tk.Frame):
 
         remember_box = tk.Checkbutton(self, text = "Lembrar deste usuário", font = ("Montserrat",10), bg = teal_Green, fg = "#002e29", activebackground = teal_Green)
         remember_box.deselect()
-        remember_box.grid(row = 3, column = 0, sticky = 'w', padx = 4)
+        remember_box.grid(row = 5, column = 0, sticky = 'w', padx = 4)
 
         #Botão de login
         def login():
+            username = username_in.get()
+            password = password_in.get()
+            if (username == "Usuário"): #Não foi inserido usuário
+                #Adiciona mensagem de erro e organiza widgets
+                u_errormsg = tk.Label(self, text = "* Inserir usuário", font = ("Montserrat",10), bg = teal_Green, fg = "#b80018")
+                u_errormsg.grid(row = 2, column = 0, columnspan = 2, sticky = 'w' + 'n', padx = 30, pady = 0)
+                username_in.grid(row = 1, column = 0, columnspan = 2, sticky = 'w', padx = 32, pady = (10,0))
+                user_icon.grid(row = 1, column = 0, sticky = 'w', pady = (10,0))
+                password_in.grid(row = 3, column = 0, columnspan = 2, sticky = 'n'+'w'+'e'+'s', padx = 32, pady = (0,30))
+                password_icon.grid(row = 3, column = 0, sticky = 'w' + 'n', pady = (25,5))
+            if (password == "Senha"): #Não foi inserido senha tambem
+                p_errormsg = tk.Label(self, text = "* Inserir senha", font = ("Montserrat",10), bg = teal_Green, fg = "#b80018")
+                p_errormsg.grid(row = 3, column = 0, columnspan = 2, sticky = 'w' + 'n', padx = 30, pady = 0)
+                password_in.grid(row = 2, column = 0, columnspan = 2, sticky = 'n'+'w'+'e', padx = 32, pady = (30,0), ipady = 0)
+                password_icon.grid(row = 2, column = 0, sticky = 'w' + 'n', pady = (28,0))            
             return
 
         login_btn = tk.Button(self, text = "Login", command = login, font = ("Montserrat"), bg = forest_Green, activebackground =  "#01695d")
-        login_btn.grid(row = 4, column = 0, columnspan = 2, sticky = 'w', padx = 2, pady = 10)
+        login_btn.grid(row = 6, column = 0, columnspan = 2, sticky = 'w', padx = 2, pady = 10)
         login_btn.configure(height = 1, width = 33)
 
         #Botão de Registrar-se
         register_btn = tk.Button(self, text = "Criar uma conta", command = lambda: controller.show_frame("RegisterWindow"),
                                 font = ("Montserrat"), bg = teal_Green, fg = "#002e29", activebackground =  teal_Green, activeforeground = black, borderwidth = 0)
-        register_btn.grid(row = 5, column = 0, columnspan = 2, sticky = 'w' + 'n', padx = 105, pady = 10)
+        register_btn.grid(row = 7, column = 0, columnspan = 2, sticky = 'w' + 'n', padx = 105, pady = 10)
 
 class RegisterWindow(tk.Frame):
     def __init__(self, master, controller):
@@ -155,7 +187,23 @@ class RegisterWindow(tk.Frame):
         new_email.insert(0, "E-mail")
         new_email.bind("<FocusIn>", holder_UText)
         new_email.bind("<FocusOut>", holder_UText)
-        new_email.grid(row = 2, column = 0, sticky = "w", padx = 70, pady = 10)
+        new_email.grid(row = 2, column = 0, sticky = "w", padx = 70, pady = (0,14))
+
+        #Criando a entrada para o Nome
+        def holder_UText(event):
+            current = new_name.get()
+            if current == "Nome":
+                new_name.delete(0, tk.END)
+                new_name.config(fg = black)
+            elif current == "":
+                new_name.insert(0, "Nome")
+                new_name.config(fg = "#c2c2c2")
+
+        new_name = tk.Entry(self, font = ("Montserrat"), fg = "#c2c2c2", width = 25)
+        new_name.insert(0, "Nome")
+        new_name.bind("<FocusIn>", holder_UText)
+        new_name.bind("<FocusOut>", holder_UText)
+        new_name.grid(row = 3, column = 0, columnspan = 2, sticky = 'w', padx = 70, pady = (5,14))
 
         #Criando a entrada para o Username
         def holder_UText(event):
@@ -171,7 +219,7 @@ class RegisterWindow(tk.Frame):
         new_username.insert(0, "Usuário")
         new_username.bind("<FocusIn>", holder_UText)
         new_username.bind("<FocusOut>", holder_UText)
-        new_username.grid(row = 3, column = 0, columnspan = 2, sticky = 'w', padx = 70, pady = 10)
+        new_username.grid(row = 4, column = 0, columnspan = 2, sticky = 'w', padx = 70, pady = (5,14))
 
         #Criando a entrada para a senha
         def holder_PText(event):
@@ -179,16 +227,16 @@ class RegisterWindow(tk.Frame):
             if current == "Senha":
                 new_password.delete(0, tk.END)
                 new_password.config(fg = black)
+                new_password.config(show = "*")
             elif current == "":
                 new_password.insert(0, "Senha")
                 new_password.config(fg = "#c2c2c2")
-
 
         new_password = tk.Entry(self, font = ("Montserrat"), fg = "#c2c2c2", width = 25)
         new_password.insert(0, "Senha")
         new_password.bind("<FocusIn>", holder_PText)
         new_password.bind("<FocusOut>", holder_PText)
-        new_password.grid(row = 4, column = 0, sticky = "w", padx = 70, pady = 10)
+        new_password.grid(row = 5, column = 0, sticky = "w", padx = 70, pady = (5,14))
 
         #Confirmar senha
         def holder_PCText(event):
@@ -196,6 +244,7 @@ class RegisterWindow(tk.Frame):
             if current == "Confirmar senha":
                 new_passwordC.delete(0, tk.END)
                 new_passwordC.config(fg = black)
+                new_passwordC.config(show = "*")
             elif current == "":
                 new_passwordC.insert(0, "Confirmar senha")
                 new_passwordC.config(fg = "#c2c2c2")
@@ -205,13 +254,70 @@ class RegisterWindow(tk.Frame):
         new_passwordC.insert(0, "Confirmar senha")
         new_passwordC.bind("<FocusIn>", holder_PCText)
         new_passwordC.bind("<FocusOut>", holder_PCText)
-        new_passwordC.grid(row = 5, column = 0, sticky = "w", padx = 70, pady = (10,26))
+        new_passwordC.grid(row = 6, column = 0, sticky = "w", padx = 70, pady = (5,20))
+        
+        #Mensagem de erro
+        error_msg = tk.Label(self, text = "", background = teal_Green, fg = red)
+        error_msg.grid(row = 7, column = 0, sticky = "w", padx = 70)
 
         #Botão para criar a conta
         def criar_Conta():
+            #Confirmando se o usuário prencheu as entradas necessárias
+            error_msg.config(text = "")
+            if ( new_email.get() == "E-mail" ) or (new_email.get() == ""):
+                new_passwordC.grid(row = 6, column = 0, sticky = "w", padx = 70, pady = (5,2))
+                error_msg.config(text = "* Inserir e-mail")
+                return
+            elif ( new_name.get() == "Nome") or (new_name.get() == ""):
+                new_passwordC.grid(row = 6, column = 0, sticky = "w", padx = 70, pady = (5,2))
+                error_msg.config(text = "* Inserir nome")
+                return
+            elif ( new_username.get() == "Usuário" ) or (new_username.get() == ""):
+                new_passwordC.grid(row = 6, column = 0, sticky = "w", padx = 70, pady = (5,2))
+                error_msg.config(text = "* Inserir usuário")
+                return
+            elif ( new_password.get() == "Senha" ) or (new_password.get() == ""):
+                new_passwordC.grid(row = 6, column = 0, sticky = "w", padx = 70, pady = (5,2))
+                error_msg.config(text = "* Inserir senha")
+                return
+            elif ( new_passwordC.get() == "Confirmar senha" ) or (new_passwordC.get() == ""):
+                new_passwordC.grid(row = 6, column = 0, sticky = "w", padx = 70, pady = (5,2))
+                error_msg.config(text = "* Confirme a senha")
+                return
+            #Confirmando se as entradas são validas  
+            elif not re.match(r"[^@]+@[^@]+\.[^@]+", new_email.get()):
+                new_passwordC.grid(row = 6, column = 0, sticky = "w", padx = 70, pady = (5,2))
+                error_msg.config(text = "* Email invalido")
+                return
+            elif (new_password.get() != new_passwordC.get()):
+                new_passwordC.grid(row = 6, column = 0, sticky = "w", padx = 70, pady = (5,2))
+                error_msg.config(text = "* Senhas não combinam")
+                return
+            #Verificando se e-mail já existe
+            cursor.execute("SELECT email, COUNT(*) FROM Users WHERE email = %s", (new_email.get(),) )
+            msg_e = cursor.fetchone()  
+            if msg_e:
+                new_passwordC.grid(row = 6, column = 0, sticky = "w", padx = 70, pady = (5,2))
+                error_msg.config(text = "* Este e-mail já esta sendo usado")
+                return
+            #Verificando se usuário já existe
+            cursor.execute("SELECT username, COUNT(*) FROM Users WHERE username = %s", (new_username.get(),) )
+            msg_u = cursor.fetchone()  
+            if msg_u:
+                new_passwordC.grid(row = 6, column = 0, sticky = "w", padx = 70, pady = (5,2))
+                error_msg.config(text = "* Este usuário já esta sendo usado")
+                return
+            #Está tudo certo, inserir usuário no db
+            add_user = """INSERT INTO Users
+                        (email, name, username, password)
+                        VALUES (%s, %s, %s, %s)"""
+            user_data = (str(new_email.get()), str(new_name.get()), str(new_username.get()), str(new_password.get()) )
+            cursor.execute(add_user, user_data)
+            db.commit()
             return
+                
         register_btn = tk.Button(self, text = "Criar", command = criar_Conta, font = ("Montserrat"), bg = forest_Green, activebackground =  "#01695d", width = 27)
-        register_btn.grid(row = 6, column = 0, columnspan = 2, sticky = 'w', padx = 69)
+        register_btn.grid(row = 8, column = 0, columnspan = 2, sticky = 'w', padx = 69)
 
 class ForgotPWindow(tk.Frame):
     def __init__(self, master, controller):
@@ -251,6 +357,26 @@ class ForgotPWindow(tk.Frame):
         register_btn = tk.Button(self, text = "Recuperar senha", command = criar_Conta, font = ("Montserrat"), bg = forest_Green, activebackground =  "#01695d", width = 27)
         register_btn.grid(row = 6, column = 0, columnspan = 2, pady = 10)
 
+#Metodo para centralizar a tela quando abrir o programa
+def center(win):
+    """
+    centers a tkinter window
+    :param win: the main window or Toplevel window to center
+    """
+    win.update_idletasks()
+    width = win.winfo_width()
+    frm_width = win.winfo_rootx() - win.winfo_x()
+    win_width = width + 2 * frm_width
+    height = win.winfo_height()
+    titlebar_height = win.winfo_rooty() - win.winfo_y()
+    win_height = height + titlebar_height + frm_width
+    x = win.winfo_screenwidth() // 2 - win_width // 2
+    y = win.winfo_screenheight() // 2 - win_height // 2
+    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+    win.deiconify()
+
 if __name__ == "__main__":
     app = BugHutner()
+    center(app)
+    app.attributes('-alpha', 1.0)
     app.mainloop()
