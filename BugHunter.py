37953756691f8804b9170661f3e6afbe47a1c772
Leonardo Meireles <62730338+LeonardoMeireles@users.noreply.github.com>
2021-05-13 01:23:03 -0300

    Add files via upload

--- BugHunter.py
+++ BugHunter.py
@@ -1,7 +1,5 @@
 try:
-    import hashlib
     import os
-    import secrets
     import tkinter as tk
     from os.path import dirname, join
     import sys
@@ -13,13 +11,9 @@ except Exception as e:
     print("Algumas bibliotecas estão faltando: {}".format(e))
 
 #Login Window
-from BH_Login.LoginWindow import *
-from BH_Login.RegisterWindow import *
-from BH_Login.ForgotPWindow import *
-
-#Home Window
-from BH_Home.HeaderMenu import *
-from BH_Home.HomeWindow import *
+from BH_Login.LoginWindow import LoginWindow
+from BH_Login.RegisterWindow import RegisterWindow
+from BH_Login.ForgotPWindow import ForgotPWindow
 
 dotenv_file = join(dirname(__file__), '.env')
 load_dotenv(dotenv_file)
@@ -40,6 +34,7 @@ DB_NAME = os.environ.get("DB_NAME")
 DB_PORT = os.environ.get("DB_PORT")
 
 #Conectando ao Banco de Dados
+global db
 db = mysql.connector.connect(
         host=DB_HOST,
         user=DB_USER,
@@ -50,15 +45,19 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 
-#Criando a Table para guardar informações de Login
+#Criando as Tabelas
 #cursor.execute("CREATE TABLE Users (user_id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255), name VARCHAR(255), username VARCHAR(50), salt VARCHAR(16), hash VARCHAR(512))")
-#cursor.execute("CREATE TABLE Projects (project_id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), category VARCHAR(50))")
+#cursor.execute("CREATE TABLE Projects (project_id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), description VARCHAR(500), category VARCHAR(50), status VARCHAR(50), date DATE)")
+#cursor.execute("CREATE TABLE Problems (problem_id INT AUTO_INCREMENT PRIMARY KEY, project_id INT(4), name VARCHAR(255), reporter VARCHAR(255), status VARCHAR(50), priority VARCHAR(50), description VARCHAR(1000), date DATE)")
+#cursor.execute("CREATE TABLE User_Projects (entry_id INT AUTO_INCREMENT PRIMARY KEY, user_id INT(4), project_id INT(4))")
+#cursor.execute("CREATE TABLE Activity (activity_id INT AUTO_INCREMENT PRIMARY KEY, type INT(2), user_id INT(4), problem_id INT(4), project_id INT(4), date DATE)")
+#cursor.execute("CREATE TABLE Comments (comment_id INT AUTO_INCREMENT PRIMARY KEY, comment VARCHAR(1000), problem_id INT(4), user_id INT(4), date DATE)")
 
 class BugHunter(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-
+        self.db = db
         width_screen = self.winfo_screenwidth()
         height_value = self.winfo_screenheight()
 
@@ -89,29 +88,29 @@ class BugHunter(tk.Tk):
 
         self.title("Bug Hunter")
         self.iconbitmap("Assets/bh_Icon.ico")
-        self.geometry("800x800")
+        self.geometry("400x400")
         self.resizable(width = 0, height = 0)
 
         self.state = False #boolean pra mostrar se está me fullscreen ou n
 
         #O container ira guardar uma pilha de frames, que podera ser modificado, para que a página que desejamos fique no topo
         global container
-        container = tk.Frame(self) 
-        container.configure(background = black)
-        container.pack(side="top", fill="both", expand=True)
-        container.columnconfigure(0, weight=1)
-        container.rowconfigure(0, weight=1)
+        self.container = tk.Frame(self) 
+        self.container.configure(background = black)
+        self.container.pack(side="top", fill="both", expand=True)
+        self.container.columnconfigure(0, weight=1)
+        self.container.rowconfigure(0, weight=1)
 
         #Os frames do programa
         self.frames = {}
-        self.frames["LoginWindow"] = LoginWindow(master = container, controller = self, BugHunter = self)
-        self.frames["LoginWindow"].grid(row = 0, column = 0, padx=190, pady=200, sticky = "nsew") #Fora do frame
+        self.frames["LoginWindow"] = LoginWindow(master = self.container, controller = self, BugHunter = self)
+        self.frames["LoginWindow"].grid(row = 0, column = 0, sticky = "nsew")
 
-        self.frames["RegisterWindow"] = RegisterWindow(master = container, controller = self)
-        self.frames["RegisterWindow"].grid(row = 0, column = 0, padx=190, pady=200, sticky = "nsew") #Fora do frame
+        self.frames["RegisterWindow"] = RegisterWindow(master = self.container, controller = self)
+        self.frames["RegisterWindow"].grid(row = 0, column = 0, sticky = "nsew")
 
-        self.frames["ForgotPWindow"] = ForgotPWindow(master = container, controller = self)
-        self.frames["ForgotPWindow"].grid(row = 0, column = 0, padx=190, pady=200, sticky = "nsew") #Fora do frame
+        self.frames["ForgotPWindow"] = ForgotPWindow(master = self.container, controller = self)
+        self.frames["ForgotPWindow"].grid(row = 0, column = 0, sticky = "nsew")
         
 
 
@@ -119,23 +118,43 @@ class BugHunter(tk.Tk):
 
     def enter_BH(self, user_id):
         #Excluido os frames de login
-        self.frames["LoginWindow"].grid_forget()
-        self.frames["LoginWindow"].destroy()
         self.frames["RegisterWindow"].grid_forget()
         self.frames["RegisterWindow"].destroy()
         self.frames["ForgotPWindow"].grid_forget()
         self.frames["ForgotPWindow"].destroy()
 
+        #Home Window
+        from BH_Home.HeaderMenu import HeaderMenu
+        from BH_Home.HomeWindow import HomeWindow
+        from BH_Home.AllProjectsWindow import AllProjectsWindow
+        from BH_Home.NewProjectWindow import NewProjectWindow
+        from BH_Home.NewProblemWindow import NewProblemWindow
+
+        #Adicionando o Header que sera presente em todos as outras páginas do BH
+        self.frames["HeaderMenu"] = HeaderMenu(master = self.container, controller = self, bH = self, user_id = user_id)
+        self.frames["HeaderMenu"].grid(row = 0, column = 0, sticky = "new")
+
+        self.frames["HomeWindow"] = HomeWindow(master = self.container, controller = self, bH = self, user_id = user_id)
+        self.frames["HomeWindow"].grid(row = 0, column = 0, sticky = "nsew", pady = (53, 0))
+
+        self.frames["AllProjectsWindow"] = AllProjectsWindow(master = self.container, controller = self, bH = self, user_id = user_id)
+        self.frames["AllProjectsWindow"].grid(row = 0, column = 0, sticky = "nsew", pady = (53, 0))
+
+        self.frames["NewProjectWindow"] = NewProjectWindow(master = self.container, controller = self, bH = self, user_id = user_id)
+        self.frames["NewProjectWindow"].grid(row = 0, column = 0, sticky = "nsew", pady = (53, 0))
+
+        self.frames["NewProblemWindow"] = NewProblemWindow(master = self.container, controller = self, bH = self, user_id = user_id)
+        self.frames["NewProblemWindow"].grid(row = 0, column = 0, sticky = "nsew", pady = (53, 0))
+
+        self.frames["LoginWindow"].grid_forget()
+        self.frames["LoginWindow"].destroy()
+
         #Entrando no BH
         self.resizable(True, True)
         self.geometry("1000x800")
-        #Adicionando o Header que sera presente em todos as outras páginas do BH
-        self.frames["HeaderMenu"] = HeaderMenu(master = container, controller = self, bH = self, user_id = user_id)
-        self.frames["HeaderMenu"].grid(row = 0, column = 0, sticky = "new")
-        self.frames["HomeWindow"] = HomeWindow(master = container, controller = self, bH = self, user_id = user_id)
-        self.frames["HomeWindow"].grid(row = 0, column = 0, sticky = "nsew", pady = (53, 0))
-
         self.show_frame("HomeWindow")
+        center(self)
+
         return
     
     #Mostra o frame de acordo com o nome passado
